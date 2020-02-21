d849f32e98a52345fdc71fee636e99832c4ae72c
Krystal Guzman <kryguzman@gmail.com>
2020-02-21 10:03:57 -0800

    Updates

---
+++ precache-manifest.2bdc81faba6cecfed03ed9f3db08bf6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bbb4988151b0d5d8d6932dd43938e9",
+    "url": "/Redux-App/index.html"
+  },
+  {
+    "revision": "f4f16cc251d2ad26125f",
+    "url": "/Redux-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eebee15341904d65fdd7",
+    "url": "/Redux-App/static/js/2.8c7e8e16.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Redux-App/static/js/2.8c7e8e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f16cc251d2ad26125f",
+    "url": "/Redux-App/static/js/main.f809d536.chunk.js"
+  },
+  {
+    "revision": "6d25752e071105aa72c0",
+    "url": "/Redux-App/static/js/runtime-main.f677dce6.js"
+  }
+]);
