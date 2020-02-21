e3e06cc95010d0233669564b3582cdc0f97d0ead
Krystal Guzman <kryguzman@gmail.com>
2020-02-21 09:51:40 -0800

    trying to push to gh pages

--- src/index.js
+++ src/index.js
@@ -19,4 +19,4 @@ ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, doc
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
